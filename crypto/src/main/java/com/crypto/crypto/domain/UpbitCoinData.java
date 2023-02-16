6af8f0fabcdc76c98f31faf4882fa2aaf9f6e8a4
w0ntae <bitcampcoding@gmail.com>
2023-02-16 17:37:44 +0900

    refactor
    Refactored Upbit entity to remove unnecessary data

--- crypto/src/main/java/com/crypto/crypto/domain/UpbitCoinData.java
+++ crypto/src/main/java/com/crypto/crypto/domain/UpbitCoinData.java
@@ -19,57 +19,36 @@ public class UpbitCoinData {
   private Long id;
   
   private String coin;
-  private String market;
   private LocalDateTime candleDateTimeUtc;
-  private LocalDateTime candleDateTimeKst;
   private String openingPrice;
   private String highPrice;
   private String lowPrice;
   private String tradePrice;
   private String timestamp;
-  private String candleAccTradePrice;
-  private String candleAccTradeVolume;
-  private String prevClosingPrice;
-  private String changePrice;
-  private String changeRate;
+ 
   
   @Builder
-  public UpbitCoinData(String coin, String market, LocalDateTime candleDateTimeUtc, LocalDateTime candleDateTimeKst,
-                       String openingPrice, String highPrice, String lowPrice, String tradePrice, String timestamp,
-                       String candleAccTradePrice, String candleAccTradeVolume, String prevClosingPrice,
-                       String changePrice, String changeRate) {
+  public UpbitCoinData(String coin,  LocalDateTime candleDateTimeUtc,
+                       String openingPrice, String highPrice, String lowPrice,
+                       String tradePrice, String timestamp) {
     this.coin = coin;
-    this.market = market;
     this.candleDateTimeUtc = candleDateTimeUtc;
-    this.candleDateTimeKst = candleDateTimeKst;
     this.openingPrice = openingPrice;
     this.highPrice = highPrice;
     this.lowPrice = lowPrice;
     this.tradePrice = tradePrice;
     this.timestamp = timestamp;
-    this.candleAccTradePrice = candleAccTradePrice;
-    this.candleAccTradeVolume = candleAccTradeVolume;
-    this.prevClosingPrice = prevClosingPrice;
-    this.changePrice = changePrice;
-    this.changeRate = changeRate;
   }
   
   public static UpbitCoinData createFromUpbitCoinDataDTO(UpbitCoinDataDTO upbitCoinDataDTO){
     UpbitCoinData upbitCoinData = UpbitCoinData.builder()
             .coin(upbitCoinDataDTO.getCoin())
-            .market(upbitCoinDataDTO.getMarket())
             .candleDateTimeUtc(upbitCoinDataDTO.getCandleDateTimeUtc())
-            .candleDateTimeKst(upbitCoinDataDTO.getCandleDateTimeKst())
             .openingPrice(upbitCoinDataDTO.getOpeningPrice())
             .highPrice(upbitCoinDataDTO.getHighPrice())
             .lowPrice(upbitCoinDataDTO.getLowPrice())
             .tradePrice(upbitCoinDataDTO.getTradePrice())
             .timestamp(upbitCoinDataDTO.getTimestamp())
-            .candleAccTradePrice(upbitCoinDataDTO.getCandleAccTradePrice())
-            .candleAccTradeVolume(upbitCoinDataDTO.getCandleAccTradeVolume())
-            .prevClosingPrice(upbitCoinDataDTO.getPrevClosingPrice())
-            .changePrice(upbitCoinDataDTO.getChangePrice())
-            .changeRate(upbitCoinDataDTO.getChangeRate())
             .build();
     return upbitCoinData;
   }
